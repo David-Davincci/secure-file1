19fe647ee73d49cf0655acb68604d7de93e2f2bc
David_Davincci <ekedavid61@gmail.com>
2025-12-02 02:47:29 +0000

    Fix Vercel deployment - configure static file serving and API routing

--- api/index.js
+++ api/index.js
@@ -1,4 +1,8 @@
 // Vercel serverless function entry point
 const app = require('../server');
 
-module.exports = app;
+// Export the Express app as a serverless function handler
+module.exports = async (req, res) => {
+    // Let Express handle the request
+    return app(req, res);
+};
